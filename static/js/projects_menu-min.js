5fa4c13d286d31967f7b824bbc6c7b497a8be411
Simon Stanley <simon.stanley87@gmail.com>
2016-12-07 15:31:53 +0000

    V1 patch3

---
+++ static/js/projects_menu-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".project-item a").hover(function(){$(this).closest(".project-item").css("border-color","#12e4ff"),$(this).siblings().css("color","#12e4ff")},function(){$(this).closest(".project-item").css("border-color","#454545"),$(this).siblings().css("color","#ffffff")})});
